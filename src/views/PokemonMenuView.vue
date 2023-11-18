9249ae78d4ae19dbdb7dbaa8ebe87a98a96e1d23
MescottSE <mescottbusiness@gmail.com>
2023-11-18 00:23:53 +0000

    Pokemon type filter fully functional

--- src/views/PokemonMenuView.vue
+++ src/views/PokemonMenuView.vue
@@ -21,7 +21,7 @@ import TheWelcome from '../components/TheWelcome.vue'
         </div>
       </div>
       <div class="row">
-        <div class="col-lg-3" v-for="(pokemon,index) in pokemonList" :key="index" @click="deletePoke($event)">
+        <div class="col-lg-3 poke-column" v-for="(pokemon,index) in pokemonList" :key="index">
           <div class="poke-block">
             <img v-if="index < 10" :src="`src/assets/pokemonIcons/00${index+1}.png`" />
             <img v-if="index >= 9 && index < 100" :src="`src/assets/pokemonIcons/0${index+1}.png`"/>
@@ -60,14 +60,37 @@ import TheWelcome from '../components/TheWelcome.vue'
         
         filterDropdown.classList.toggle("toggler");
 
+        this.hideShowPokemon(selected.innerText.toLowerCase());
+      },
+
+      hideShowPokemon(typePoke){
+        let pokemonColumns = document.querySelectorAll('.poke-column');
+
+        
+        if(typePoke === "all types"){
+          pokemonColumns.forEach(element => {
+            element.style.display = "block";
+          });
+        }else{
+          pokemonColumns.forEach(element => {
+            let pokeType = element.querySelector("." + typePoke + "-type");
+            
+            console.log(pokeType);
+            
+            if(pokeType){
+              element.style.display = "block";
+            }else{
+              element.style.display = "none";
+            }
+            
+          });
+        }
       },
 
       toggler(event){
         let element = event.currentTarget;
         let targetSibling = element.nextElementSibling;
         let delay = 500;
-
-        console.log(targetSibling.style.maxHeight);
         
         targetSibling.classList.toggle("toggler")
       },
