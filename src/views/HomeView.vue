141b94ba482894d1be038945ab32365f80f94d30
MescottSE <mescottbusiness@gmail.com>
2023-11-28 22:18:03 +0000

    HomePage initial setup and map canvas

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,37 @@
+
 <script setup>
 
 </script>
 
 <template>
-  <main>
-    
+  <main class="poke-home-component">
+    <canvas id="poke-map" ref="pokeMap" class="poke-map"></canvas>
   </main>
 </template>
+
+  
+  <script>
+  
+    export default {
+      
+      data(){
+        return{
+          
+        }
+      },
+
+      methods:{
+
+        canvasSetup(){
+          const canvas = this.$refs.pokeMap;
+          canvas.width = window.innerWidth;
+          canvas.height = window.innerHeight / 2;
+        }
+      },
+
+      mounted(){
+        this.canvasSetup();
+      }
+    }
+  </script>
+  
