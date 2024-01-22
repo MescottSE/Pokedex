77c7455160889bfff7525888ce4c12f3583327ba
MescottSE <mescottbusiness@gmail.com>
2024-01-22 22:42:11 +0000

    World moves in all directions

--- src/views/BattleView.vue
+++ src/views/BattleView.vue
@@ -19,10 +19,34 @@
   import dawnImage from '@/assets/playerImage/dawn.png';
   import gameBackground from '@/assets/maps/AZIXfinal.png';
 
+  class World{
+    constructor(x, y){
+      this.x = x;
+      this.y = y;
+    }
+
+    setX(x){
+      this.x = this.x + x;
+    }
+
+    setY(y){
+      this.y = this.y + y;
+    }
+
+    getX(){
+      return this.x;
+    }
+
+    getY(){
+      return this.y;
+    }
+  }
+
   export default {
     data() {
       return {
-        blockDimension: 40 // Adjust the player image size here
+        blockDimension: 40, // Adjust the player image size here
+        world: new World(410,0)
       };
     },
 
@@ -31,6 +55,7 @@
       this.initializeMap();
       this.initializePlayer();
       window.addEventListener("resize", this.initiateCanvas);
+      window.addEventListener("keydown", this.arrowsDown);
       requestAnimationFrame(this.gameLoop);
     },
 
@@ -62,12 +87,58 @@
           const canvas = this.$refs.battleScreen;
           const ctx = canvas.getContext('2d');
 
-          ctx.drawImage(mapImage, 0,0, 1000, 1000);
+          ctx.drawImage(mapImage, this.world.getX(), this.world.getY(), 1000, 1000);
         };
       },
 
+      arrowsDown(event){
+        event.preventDefault();
+
+        let x = this.world.getX();
+        let y = this.world.getY();
+
+        let moveAmount = 20;
+
+        switch(event.key){
+          case 'ArrowUp':
+            console.log("Move Up");
+            this.world.setY(moveAmount);
+            break;
+          case 'ArrowDown':
+            console.log("Move Down");
+            this.world.setY(-moveAmount);
+            break;
+          case 'ArrowLeft':
+            console.log("Move Left");
+            this.world.setX(moveAmount);
+            break;
+          case 'ArrowRight':
+            console.log("Move Right");
+            this.world.setX(-moveAmount);
+            break;
+        }
+
+        this.move();
+      },
+
+      move(){
+        const mapImage = new Image();
+            mapImage.src = gameBackground;
+
+            mapImage.onload = ()=>{
+
+              const canvas = this.$refs.battleScreen;
+              const ctx = canvas.getContext('2d');
+  
+              ctx.clearRect(0, 0, canvas.width, canvas.height);
+              ctx.drawImage(mapImage, this.world.getX(), this.world.getY(), 1000, 1000);
+              this.initializePlayer();
+            }
+      },
+
       gameLoop() {
-        // Add your game logic here
+        
+        this.move();
       }
     }
   }
